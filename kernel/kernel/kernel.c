f3a73f62acdf9975d590749505524e34c3596a8e
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-02-10 14:01:43 -0500

    attempt at logo in ascii "art"

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 #include <kernel/io.h>
-
 #include <kernel/tty.h>
+#include <kernel/vga.h>
 #include <kernel/ps2keyboard.h>
 
 void kernel_early(void)
@@ -16,6 +16,15 @@ void kernel_early(void)
 void kernel_main(void)
 {
 	printf("Welcome to HexOS\n");
+
+	terminal_setcolor(COLOR_GREEN);
+	printf("	_   _            ___  ____   \n");
+	printf("   | | | | _____  __/ _ \\/ ___|  \n");
+	printf("   | |_| |/ _ \\ \\/ / | | \\___ \\  \n");
+	printf("   |  _  |  __/>  <| |_| |___) | \n");
+	printf("   |_| |_|\\___/_/\\_\\___/|____/  \n");
+	terminal_setcolor(COLOR_WHITE);
+
 	while (1) {
 		printf("%c", getchar());
 	}
