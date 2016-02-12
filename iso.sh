ef99e165f71ffc272fdd16d0d40e8e3750a9776c
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-02-11 19:29:47 -0500

    that beep got annoying

--- iso.sh
+++ iso.sh
@@ -9,8 +9,7 @@ mkdir -p isodir/boot/grub
 cp sysroot/boot/hexos.kernel isodir/boot/hexos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "HexOS (serial debug on COM1)" {
-	multiboot /boot/hexos.kernel
-	play 60 440 1
+	multiboot /boot/hexos.kernel -serdbg
 }
 menuentry "HexOS" {
 	multiboot /boot/hexos.kernel
